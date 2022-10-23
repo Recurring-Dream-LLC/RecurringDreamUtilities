eed884d40276e68c4fb2395d21d17ac442008e04
Jeff Hokit <jhokit@users.noreply.github.com>
2022-10-23 14:06:37 -0700

    Initial Commit

---
+++ Sources/RecurringDreamUtilities/RecurringDreamUtilities.swift
@@ -0,0 +1,6 @@
+public struct RecurringDreamUtilities {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/RecurringDreamUtilitiesTests/RecurringDreamUtilitiesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RecurringDreamUtilities
+
+final class RecurringDreamUtilitiesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RecurringDreamUtilities().text, "Hello, World!")
+    }
+}
